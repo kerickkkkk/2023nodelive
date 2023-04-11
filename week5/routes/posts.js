2c18e52a618c940bf8a53ccf81ce71c627c960a8
eric <howcrazys@gmail.com>
2023-04-11 22:07:13 +0800

    feat: week5 init

---
+++ week5/routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const postsController = require('../controllers/postsController')
+
+/* GET users listing. */
+router.get('/posts', postsController.getPosts);
+router.post('/post', postsController.postPosts);
+
+module.exports = router;
