8dc7c5c7ea16a045815555fd2b9357a5955aad00
eric <howcrazys@gmail.com>
2023-04-15 23:41:43 +0800

    feat: 調整路由

--- week6/routes/users.js
+++ week6/routes/users.js
@@ -4,17 +4,18 @@ const User = require('../models/userModel')
 const userController = require('../controllers/usersController')
 const {isAuth} = require('../middleware/auth')
 /* GET users listing. */
-// 取得所有人員
-router.get('/users', isAuth, userController.getUsers);
+// 取得所有人員 無驗證
+router.get('/users', userController.getUsers);
 // 註冊
-router.post('/user', userController.singUp);
-
-// 確認是否有權限 並取得使用者資訊
-router.get('/user/check', isAuth, userController.checkUser)
-
-router.post('/user/reset_password', isAuth, userController.resetPassword)
-
+router.post('/users/sign_up', userController.singUp);
 router.post('/login', userController.login)
+// 重置密碼
+router.post('/users/reset_password', isAuth, userController.resetPassword)
+// 確認是否有權限 並取得使用者資訊
+router.get('/users/profile', isAuth, userController.profile)
+router.patch('/users/profile', isAuth, userController.updateProfile)
+
+
 
 router.post('/')
 module.exports = router;
